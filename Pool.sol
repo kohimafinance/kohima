46228f205c643808791dadefe2b6dadb37f8bed7
kohimafinance <77331601+kohimafinance@users.noreply.github.com>
2021-01-28 15:53:39 +0530

    Add files via upload

--- Pool.sol
+++ Pool.sol
@@ -82,6 +82,280 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
+/**
+ * @dev Collection of functions related to the address type
+ */
+library Address {
+    /**
+     * @dev Returns true if `account` is a contract.
+     *
+     * [IMPORTANT]
+     * ====
+     * It is unsafe to assume that an address for which this function returns
+     * false is an externally-owned account (EOA) and not a contract.
+     *
+     * Among others, `isContract` will return false for the following
+     * types of addresses:
+     *
+     *  - an externally-owned account
+     *  - a contract in construction
+     *  - an address where a contract will be created
+     *  - an address where a contract lived, but was destroyed
+     * ====
+     */
+    function isContract(address account) internal view returns (bool) {
+        // This method relies on extcodesize, which returns 0 for contracts in
+        // construction, since the code is only stored at the end of the
+        // constructor execution.
+
+        uint256 size;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+
+    /**
+     * @dev Replacement for Solidity's `transfer`: sends `amount` wei to
+     * `recipient`, forwarding all available gas and reverting on errors.
+     *
+     * https://eips.ethereum.org/EIPS/eip-1884[EIP1884] increases the gas cost
+     * of certain opcodes, possibly making contracts go over the 2300 gas limit
+     * imposed by `transfer`, making them unable to receive funds via
+     * `transfer`. {sendValue} removes this limitation.
+     *
+     * https://diligence.consensys.net/posts/2019/09/stop-using-soliditys-transfer-now/[Learn more].
+     *
+     * IMPORTANT: because control is transferred to `recipient`, care must be
+     * taken to not create reentrancy vulnerabilities. Consider using
+     * {ReentrancyGuard} or the
+     * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
+     */
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+
+        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
+        (bool success, ) = recipient.call{ value: amount }("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+
+    /**
+     * @dev Performs a Solidity function call using a low level `call`. A
+     * plain`call` is an unsafe replacement for a function call: use this
+     * function instead.
+     *
+     * If `target` reverts with a revert reason, it is bubbled up by this
+     * function (like regular Solidity function calls).
+     *
+     * Returns the raw returned data. To convert to the expected return value,
+     * use https://solidity.readthedocs.io/en/latest/units-and-global-variables.html?highlight=abi.decode#abi-encoding-and-decoding-functions[`abi.decode`].
+     *
+     * Requirements:
+     *
+     * - `target` must be a contract.
+     * - calling `target` with `data` must not revert.
+     *
+     * _Available since v3.1._
+     */
+    function functionCall(address target, bytes memory data) internal returns (bytes memory) {
+      return functionCall(target, data, "Address: low-level call failed");
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`], but with
+     * `errorMessage` as a fallback revert reason when `target` reverts.
+     *
+     * _Available since v3.1._
+     */
+    function functionCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
+        return functionCallWithValue(target, data, 0, errorMessage);
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
+     * but also transferring `value` wei to `target`.
+     *
+     * Requirements:
+     *
+     * - the calling contract must have an ETH balance of at least `value`.
+     * - the called Solidity function must be `payable`.
+     *
+     * _Available since v3.1._
+     */
+    function functionCallWithValue(address target, bytes memory data, uint256 value) internal returns (bytes memory) {
+        return functionCallWithValue(target, data, value, "Address: low-level call with value failed");
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCallWithValue-address-bytes-uint256-}[`functionCallWithValue`], but
+     * with `errorMessage` as a fallback revert reason when `target` reverts.
+     *
+     * _Available since v3.1._
+     */
+    function functionCallWithValue(address target, bytes memory data, uint256 value, string memory errorMessage) internal returns (bytes memory) {
+        require(address(this).balance >= value, "Address: insufficient balance for call");
+        require(isContract(target), "Address: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.call{ value: value }(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
+     * but performing a static call.
+     *
+     * _Available since v3.3._
+     */
+    function functionStaticCall(address target, bytes memory data) internal view returns (bytes memory) {
+        return functionStaticCall(target, data, "Address: low-level static call failed");
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-string-}[`functionCall`],
+     * but performing a static call.
+     *
+     * _Available since v3.3._
+     */
+    function functionStaticCall(address target, bytes memory data, string memory errorMessage) internal view returns (bytes memory) {
+        require(isContract(target), "Address: static call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.staticcall(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
+     * but performing a delegate call.
+     *
+     * _Available since v3.3._
+     */
+    function functionDelegateCall(address target, bytes memory data) internal returns (bytes memory) {
+        return functionDelegateCall(target, data, "Address: low-level delegate call failed");
+    }
+
+    /**
+     * @dev Same as {xref-Address-functionCall-address-bytes-string-}[`functionCall`],
+     * but performing a delegate call.
+     *
+     * _Available since v3.3._
+     */
+    function functionDelegateCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
+        require(isContract(target), "Address: delegate call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.delegatecall(data);
+        return _verifyCallResult(success, returndata, errorMessage);
+    }
+
+    function _verifyCallResult(bool success, bytes memory returndata, string memory errorMessage) private pure returns(bytes memory) {
+        if (success) {
+            return returndata;
+        } else {
+            // Look for revert reason and bubble it up if present
+            if (returndata.length > 0) {
+                // The easiest way to bubble the revert reason is using memory via assembly
+
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
+}
+
+/**
+ * @dev Standard math utilities missing in the Solidity language.
+ */
+library Math {
+    /**
+     * @dev Returns the largest of two numbers.
+     */
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    /**
+     * @dev Returns the smallest of two numbers.
+     */
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    /**
+     * @dev Returns the average of two numbers. The result is rounded towards
+     * zero.
+     */
+    function average(uint256 a, uint256 b) internal pure returns (uint256) {
+        // (a + b) / 2 can overflow, so we distribute
+        return (a / 2) + (b / 2) + ((a % 2 + b % 2) / 2);
+    }
+}
+
+library SafeERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(IERC20 token, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    /**
+     * @dev Deprecated. This function has issues similar to the ones found in
+     * {IERC20-approve}, and its usage is discouraged.
+     *
+     * Whenever possible, use {safeIncreaseAllowance} and
+     * {safeDecreaseAllowance} instead.
+     */
+    function safeApprove(IERC20 token, address spender, uint256 value) internal {
+        // safeApprove should only be called when setting an initial allowance,
+        // or when resetting it to zero. To increase and decrease it, use
+        // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
+        // solhint-disable-next-line max-line-length
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+
+    function safeIncreaseAllowance(IERC20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).add(value);
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    function safeDecreaseAllowance(IERC20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).sub(value, "SafeERC20: decreased allowance below zero");
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    /**
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must not be false).
+     * @param token The token targeted by the call.
+     * @param data The call data (encoded using abi.encode or one of its variants).
+     */
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+        // we're implementing it ourselves. We use {Address.functionCall} to perform this call, which verifies that
+        // the target address contains contract code and also asserts for success in the low-level call.
+
+        bytes memory returndata = address(token).functionCall(data, "SafeERC20: low-level call failed");
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
+}
+
+
 
 library SafeMath {
     /**
@@ -676,5 +950,191 @@ contract KohimaToken is KohimaTokenCore {
 
     function getLockStatus() view public returns(bool) {
         return _lock;
+    }
+}
+
+
+contract LPToKHM is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for KohimaToken;
+    using SafeERC20 for IERC20;
+    using Address for address;
+
+    KohimaToken private khm;
+    IERC20 private lpToken;
+
+    //LP token balances
+    mapping(address => uint256) private _lpBalances;
+    uint private _lpTotalSupply;
+
+    // halving period time
+    uint256 public constant DURATION = 2 weeks;
+    // initial amount of khm
+    uint256 public initReward = 3750 * 1e18;
+    bool public haveStarted = false;
+    // next time of halving
+    uint256 public halvingTime = 0;
+    uint256 public lastUpdateTime = 0;
+    // distribution of per second
+    uint256 public rewardRate = 0;
+    uint256 public rewardPerLPToken = 0;
+    mapping(address => uint256) private rewards;
+    mapping(address => uint256) private userRewardPerTokenPaid;
+
+
+    // Something about dev.
+    address public devAddr;
+    uint256 public devDistributeRate = 0;
+    uint256 public lastDistributeTime = 0;
+    uint256 public devFinishTime = 0;
+    uint256 public devFundAmount = 500000 * 1e18;
+    uint256 public devDistributeDuration = 1825 days;
+
+    event Stake(address indexed from, uint amount);
+    event Withdraw(address indexed to, uint amount);
+    event Claim(address indexed to, uint amount);
+    event Halving(uint amount);
+    event Start(uint amount);
+
+    constructor(address _khm, address _lpToken) {
+        khm = KohimaToken(_khm);
+        lpToken = IERC20(_lpToken);
+        devAddr = owner();
+    }
+
+    function totalSupply() public view returns(uint256) {
+        return _lpTotalSupply;
+    }
+
+    function balanceOf(address account) public view returns(uint256) {
+        return _lpBalances[account];
+    }
+
+    function stake(uint amount) public shouldStarted {
+        updateRewards(msg.sender);
+        checkHalving();
+        require(!address(msg.sender).isContract(), "Please use your individual account.");
+        lpToken.safeTransferFrom(msg.sender, address(this), amount);
+        _lpTotalSupply = _lpTotalSupply.add(amount);
+        _lpBalances[msg.sender] = _lpBalances[msg.sender].add(amount);
+        distributeDevFund();
+        emit Stake(msg.sender, amount);
+    }
+
+    function withdraw(uint amount) public shouldStarted {
+        updateRewards(msg.sender);
+        checkHalving();
+        require(amount <= _lpBalances[msg.sender] && _lpBalances[msg.sender] > 0, "Bad withdraw.");
+        lpToken.safeTransfer(msg.sender, amount);
+        _lpTotalSupply = _lpTotalSupply.sub(amount);
+        _lpBalances[msg.sender] = _lpBalances[msg.sender].sub(amount);
+        distributeDevFund();
+        emit Withdraw(msg.sender, amount);
+    }
+
+    function claim(uint amount) public shouldStarted {
+        updateRewards(msg.sender);
+        checkHalving();
+        require(amount <= rewards[msg.sender] && rewards[msg.sender] > 0, "Bad claim.");
+        rewards[msg.sender] = rewards[msg.sender].sub(amount);
+        khm.safeTransfer(msg.sender, amount);
+        distributeDevFund();
+        emit Claim(msg.sender, amount);
+    }
+
+    function checkHalving() internal {
+        if (block.timestamp >= halvingTime) {
+            initReward = initReward.mul(50).div(100);
+            khm.mint(address(this), initReward);
+
+            rewardRate = initReward.div(DURATION);
+            halvingTime = halvingTime.add(DURATION);
+
+            updateRewards(msg.sender);
+            emit Halving(initReward);
+        }
+    }
+
+    modifier shouldStarted() {
+        require(haveStarted == true, "Have not started.");
+        _;
+    }
+
+    function getRewardsAmount(address account) public view returns(uint256) {
+        return balanceOf(account).mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+                .div(1e18)
+                .add(rewards[account]);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (_lpTotalSupply == 0) {
+            return rewardPerLPToken;
+        }
+        return rewardPerLPToken
+            .add(Math.min(block.timestamp, halvingTime)
+                    .sub(lastUpdateTime)
+                    .mul(rewardRate)
+                    .mul(1e18)
+                    .div(_lpTotalSupply)
+            );
+    }
+
+    function updateRewards(address account) internal {
+        rewardPerLPToken = rewardPerToken();
+        lastUpdateTime = lastRewardTime();
+        if (account != address(0)) {
+            rewards[account] = getRewardsAmount(account);
+            userRewardPerTokenPaid[account] = rewardPerLPToken;
+        }
+    }
+
+    function lastRewardTime() public view returns (uint256) {
+        return Math.min(block.timestamp, halvingTime);
+    }
+
+    function startFarming() external onlyOwner {
+        updateRewards(address(0));
+        rewardRate = initReward.div(DURATION);
+
+        uint256 mintAmount = initReward.add(devFundAmount);
+        khm.mint(address(this), mintAmount);
+        devDistributeRate = devFundAmount.div(devDistributeDuration);
+        devFinishTime = block.timestamp.add(devDistributeDuration);
+
+        lastUpdateTime = block.timestamp;
+        lastDistributeTime = block.timestamp;
+        halvingTime = block.timestamp.add(DURATION);
+
+        haveStarted = true;
+        emit Start(mintAmount);
+    }
+
+    function transferDevAddr(address newAddr) public onlyDev {
+        require(newAddr != address(0), "zero addr");
+        devAddr = newAddr;
+    }
+
+    function distributeDevFund() internal {
+        uint256 nowTime = Math.min(block.timestamp, devFinishTime);
+        uint256 fundAmount = nowTime.sub(lastDistributeTime).mul(devDistributeRate);
+        khm.safeTransfer(devAddr, fundAmount);
+        lastDistributeTime = nowTime;
+    }
+
+    modifier onlyDev() {
+        require(msg.sender == devAddr, "This is only for dev.");
+        _;
+    }
+
+    function lpTokenAddress() view public returns(address) {
+        return address(lpToken);
+    }
+
+    function khmAddress() view public returns(address) {
+        return address(khm);
+    }
+
+    function testMint() public onlyOwner {
+        khm.mint(address(this), 1);
     }
 }
